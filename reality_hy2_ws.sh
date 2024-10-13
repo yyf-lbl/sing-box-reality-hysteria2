5cb1ab3aeb8662e76ca1226af5c3e9238bd8083d
yyfalbl <fyun9177@gmail.com>
2024-10-13 13:57:38 +0800

    Update reality_hy2_ws.sh

--- reality_hy2_ws.sh
+++ reality_hy2_ws.sh
@@ -134,7 +134,7 @@ show_client_configuration() {
 
         server_link="vless://$uuid@$server_ip:$current_listen_port?encryption=none&flow=xtls-rprx-vision&security=reality&sni=$current_server_name&fp=chrome&pbk=$public_key&sid=$short_id&type=tcp&headerType=none#SING-BOX-Reality"
         echo "Reality 客户端通用链接："
-        echo "$server_link"
+       echo -e "\e[1;3;33m$server_link\e[0m"
         echo ""
     fi
     # 生成 Hysteria2 客户端链接
@@ -158,7 +158,7 @@ show_client_configuration() {
         echo -e "以下端口 443 可改为 2053 2083 2087 2096 8443"
         echo ""
         vmess_link_tls='vmess://'$(echo '{"add":"speed.cloudflare.com","aid":"0","host":"'$argo'","id":"'$vmess_uuid'","net":"ws","path":"'$ws_path'","port":"443","ps":"sing-box-vmess-tls","tls":"tls","type":"none","v":"2"}' | base64 -w 0)
-        echo "$vmess_link_tls"
+        echo -e "\e[1;3;33m$vmess_link_tls\e[0m"
         echo ""
         echo -e "以下端口 80 可改为 8080 8880 2052 2082 2086 2095" 
         echo ""
