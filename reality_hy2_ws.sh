4193efbef1268d5d71210ec1defef16b76ba6255
yyfalbl <fyun9177@gmail.com>
2024-11-01 01:16:49 +0800

    Update reality_hy2_ws.sh

--- reality_hy2_ws.sh
+++ reality_hy2_ws.sh
@@ -186,23 +186,39 @@ show_client_configuration() {
          echo -e "\e[1;3;33m$hy2_server_link\e[0m"
          echo ""
     fi
-    # 生成 VMess 客户端链接
+  
+   # 判断是否存在固定隧道配置 生成 VMess 客户端链接
+if jq -e '.ingress[] | select(.service == "http://localhost:$vmess_port")' /root/sbox/tunnel.yml > /dev/null; then
+    # 存在固定隧道
+    fixed_tunnel_domain=$(jq -r '.ingress[] | select(.service == "http://localhost:$vmess_port") | .hostname' /root/sbox/tunnel.yml)
+    echo -e "\e[1;3;31m使用固定隧道生成的 Vmess 客户端通用链接\e[0m"
+    
+    # 生成固定隧道链接
+    vmess_link_tls='vmess://'$(echo '{"add":"'$fixed_tunnel_domain'","aid":"0","host":"'$argo'","id":"'$vmess_uuid'","net":"ws","path":"'$ws_path'","port":"443","ps":"sing-box-vmess-tls","tls":"tls","type":"none","v":"2"}' | base64 -w 0)
+    echo -e "\e[1;3;33m$vmess_link_tls\e[0m"
+
+    vmess_link_no_tls='vmess://'$(echo '{"add":"'$fixed_tunnel_domain'","aid":"0","host":"'$argo'","id":"'$vmess_uuid'","net":"ws","path":"'$ws_path'","port":"80","ps":"sing-box-vmess","tls":"","type":"none","v":"2"}' | base64 -w 0)
+    echo -e "\e[1;3;33m$vmess_link_no_tls\e[0m"
+
+else
+    # 不存在固定隧道，生成临时隧道链接
     if jq -e '.inbounds[] | select(.type == "vmess")' /root/sbox/sbconfig_server.json > /dev/null; then
         vmess_uuid=$(jq -r '.inbounds[] | select(.type == "vmess") | .users[0].uuid' /root/sbox/sbconfig_server.json)
         ws_path=$(jq -r '.inbounds[] | select(.type == "vmess") | .transport.path' /root/sbox/sbconfig_server.json)
         argo=$(base64 --decode /root/sbox/argo.txt.b64)
-        echo -e "\e[1;3;31mVmess 客户端通用链接，替换speed.cloudflare.com为自己的优选ip可获得极致体验\e[0m"
-       echo -e "\e[1;3;32m以下端口 443 可改为 2053 2083 2087 2096 8443\e[0m"
-        echo ""
+
+        echo -e "\e[1;3;31m使用临时隧道生成的 Vmess 客户端通用链接\e[0m"
+        echo -e "\e[1;3;32m以下端口 443 可改为 2053 2083 2087 2096 8443\e[0m"
+        
         vmess_link_tls='vmess://'$(echo '{"add":"speed.cloudflare.com","aid":"0","host":"'$argo'","id":"'$vmess_uuid'","net":"ws","path":"'$ws_path'","port":"443","ps":"sing-box-vmess-tls","tls":"tls","type":"none","v":"2"}' | base64 -w 0)
         echo -e "\e[1;3;33m$vmess_link_tls\e[0m"
-        echo ""
-        echo -e "\e[1;3;32m以下端口 80 可改为 8080 8880 2052 2082 2086 2095\e[0m" 
-        echo ""
+        
+        echo -e "\e[1;3;32m以下端口 80 可改为 8080 8880 2052 2082 2086 2095\e[0m"
+        
         vmess_link_no_tls='vmess://'$(echo '{"add":"speed.cloudflare.com","aid":"0","host":"'$argo'","id":"'$vmess_uuid'","net":"ws","path":"'$ws_path'","port":"80","ps":"sing-box-vmess","tls":"","type":"none","v":"2"}' | base64 -w 0)
-          echo -e "\e[1;3;33m$vmess_link_no_tls\e[0m"
-        echo ""
+        echo -e "\e[1;3;33m$vmess_link_no_tls\e[0m"
     fi
+fi
     
    # 生成 TUIC 客户端链接
 if jq -e '.inbounds[] | select(.type == "tuic")' /root/sbox/sbconfig_server.json > /dev/null; then
