dd54ce06282a7ba741b4d1543a466cb80e32386c
yyfalbl <fyun9177@gmail.com>
2024-11-01 22:39:12 +0800

    Update reality_hy2_ws.sh

--- reality_hy2_ws.sh
+++ reality_hy2_ws.sh
@@ -492,6 +492,7 @@ sleep 2
 echo 等待cloudflare argo生成地址
 sleep 2
 #提取域名
+strings argo.log > argo_text.log
 argo=$(grep trycloudflare.com argo.log | awk 'NR==2 {print $1}' | sed 's|https://||')
 echo "$argo" | base64 > /root/sbox/argo.txt.b64
 fi
