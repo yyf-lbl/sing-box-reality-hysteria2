3f48e8a1a734fa905d0a9ced992a160e66ed8743
yyfalbl <fyun9177@gmail.com>
2024-11-04 00:46:51 +0800

    Update reality_hy2_ws.sh

--- reality_hy2_ws.sh
+++ reality_hy2_ws.sh
@@ -6,7 +6,8 @@ add_alias() {
     [ ! -f "$config_file" ] || touch "$config_file"
     for alias_name in "${alias_names[@]}"; do
         if ! grep -q "alias $alias_name=" "$config_file" 2>/dev/null; then  
-            echo "Adding alias $alias_name to $config_file"
+        #   echo "Adding alias $alias_name to $config_file"
+            echo -e "\e[1;3;31m快捷指令已创建 a或5\e[0m"
             echo "alias $alias_name='bash <(curl -fsSL https://github.com/yyfalbl/sing-box-reality-hysteria2/raw/main/reality_hy2_ws.sh)'" >> "$config_file"
  fi
     done
