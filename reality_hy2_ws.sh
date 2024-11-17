d3810898debdc667f3e97d4c83f4dfd75932692c
yyfalbl <fyun9177@gmail.com>
2024-11-17 16:11:21 +0800

    Update reality_hy2_ws.sh

--- reality_hy2_ws.sh
+++ reality_hy2_ws.sh
@@ -612,7 +612,7 @@ done
 config="{
   \"log\": {
     \"disabled\": true,
-    \"level\": \"warn\",
+    \"level\": \"error\",
     \"timestamp\": false
   },
   \"dns\": {
@@ -620,20 +620,20 @@ config="{
       {
         \"tag\": \"cloudflare\",
         \"address\": \"https://1.1.1.1/dns-query\",
-        \"strategy\": \"ipv4_only\",
+        \"strategy\": \"prefer_ipv4\",
         \"detour\": \"direct\"
       },
       {
         \"tag\": \"google\",
         \"address\": \"tls://8.8.8.8\",
-        \"strategy\": \"ipv4_only\",
+        \"strategy\": \"prefer_ipv4\",
         \"detour\": \"direct\"
       }
     ],
-        \"final\": \"cloudflare\",  
-        \"strategy\": \"\",
-        \"disable_cache\": false,
-        \"disable_expire\": false
+    \"final\": \"cloudflare\",
+    \"strategy\": \"prefer_ipv4\",
+    \"disable_cache\": false,
+    \"disable_expire\": false
   },
   \"inbounds\": [],
   \"outbounds\": [
@@ -660,7 +660,7 @@ config="{
       ],
       \"private_key\": \"mPZo+V9qlrMGCZ7+E6z2NI6NOV34PD++TpAR09PtCWI=\",
       \"peer_public_key\": \"bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=\",
-      \"mtu\": 1350,
+      \"mtu\": 1280,
       \"reserved\": [0, 0, 0]
     }
   ],
@@ -687,7 +687,7 @@ config="{
         \"outbound\": \"block\"
       }
     ],
-    \"final\": \"google\",
+    \"final\": \"direct\",
     \"rule_set\": [
       {
         \"tag\": \"geosite-netflix\",
@@ -710,8 +710,7 @@ config="{
         \"url\": \"https://raw.githubusercontent.com/SagerNet/sing-geosite/rule-set/geosite-category-ads-all.srs\",
         \"download_detour\": \"direct\"
       }
-    ],
-    \"final\": \"direct\"
+    ]
   },
   \"experimental\": {
     \"cache_file\": {
@@ -720,7 +719,8 @@ config="{
       \"store_fakeip\": true
     }
   }
-}"
+}
+"
 
     for choice in $choices; do
         case $choice in
