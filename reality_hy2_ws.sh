04213c073b1d847e12df18681b9865bd78769cb9
yyfalbl <fyun9177@gmail.com>
2024-11-19 20:41:10 +0800

    Update reality_hy2_ws.sh

--- reality_hy2_ws.sh
+++ reality_hy2_ws.sh
@@ -67,7 +67,7 @@ regenarte_cloudflared_argo(){
 # download singbox and cloudflared
 download_singbox() {
   local arch=$(uname -m)
-  local version="1.11.0-alpha.6" # 指定版本
+  local version="1.11.0-alpha.1" # 指定版本
 
   echo "Downloading sing-box version: $version"
   echo "Detected architecture: $arch"
