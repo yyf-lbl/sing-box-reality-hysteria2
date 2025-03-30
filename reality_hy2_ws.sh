c792062a98e83ec4020469d931db3b021e0bb12d
yyf-lbl <yyfalbl@gmail.com>
2025-03-30 22:56:34 +0800

    Update reality_hy2_ws.sh

--- reality_hy2_ws.sh
+++ reality_hy2_ws.sh
@@ -439,18 +439,10 @@ fi
 #singbox 内核切换
 switch_kernel() {
     # 获取当前 sing-box 版本
-    if [[ -f /root/sbox/latest_version/sing-box-stable* ]]; then
-    # 最新正式版
-    current_version="最新正式版: $(ls /root/sbox/latest_version/sing-box-stable* | head -n1 | sed 's/.*sing-box-\(.*\)/\1/')"
-elif [[ -f /root/sbox/latest_version/sing-box-alpha* ]]; then
-    # 最新测试版
-    current_version="最新测试版: $(ls /root/sbox/latest_version/sing-box-alpha* | head -n1 | sed 's/.*sing-box-\(.*\)/\1/')"
-elif [[ -f /root/sbox/old_version/sing-box-stable* ]]; then
-    # 旧正式版
-    current_version="旧正式版: $(ls /root/sbox/old_version/sing-box-stable* | head -n1 | sed 's/.*sing-box-\(.*\)/\1/')"
-elif [[ -f /root/sbox/old_version/sing-box-alpha* ]]; then
-    # 旧测试版
-    current_version="旧测试版: $(ls /root/sbox/old_version/sing-box-alpha* | head -n1 | sed 's/.*sing-box-\(.*\)/\1/')"
+  if [[ -f /root/sbox/latest_version/sing-box-* ]]; then
+    current_version=$(ls /root/sbox/latest_version/sing-box-* | head -n1 | sed 's/.*sing-box-\(.*\)/\1/')
+elif [[ -f /root/sbox/old_version/sing-box-* ]]; then
+    current_version=$(ls /root/sbox/old_version/sing-box-* | head -n1 | sed 's/.*sing-box-\(.*\)/\1/')
 else
     current_version="未找到 sing-box"
 fi
